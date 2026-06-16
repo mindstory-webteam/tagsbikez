e32956ea25741c59dc69e9a51dfb2b584c05a8ef
Ajith k v <ajith66310@gmail.com>
2026-06-16 14:31:28 +0530

    feat: add Google Tag Manager script to RootLayout for enhanced tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Oswald, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import LenisProvider from "@/components/providers/LenisProvider";
 import Navbar from "@/components/layout/header/Navbar";
@@ -36,6 +37,9 @@ export const metadata = {
       url: "/favicon.ico",
     },
   ],
+  verification: {
+    google: "kvnVEngId6kiWU3VFQArCDKyqa75MvuDCX3K0dDVeTE",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -44,7 +48,26 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${oswald.variable} ${inter.variable} h-full antialiased`}
     >
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WV2CDTM9');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col bg-[#0a0a0a] text-white">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WV2CDTM9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <LenisProvider>
           <Navbar />
           <Breadcrumb />
