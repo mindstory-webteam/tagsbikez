37eaecc800c5ef092854e9cc6d0d7bae7354ee14
Ajith k v <ajith66310@gmail.com>
2026-07-02 12:27:02 +0530

    feat: add locations data module with royal enfield showroom list

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,10 +1,10 @@
 export const locations = [
-  { slug: "royal-enfield-dealer-chalakudy", name: "Chalakudy" },
-  { slug: "royal-enfield-dealer-irinjalakuda", name: "Irinjalakuda" },
-  { slug: "royal-enfield-dealer-kodakara", name: "Kodakara" },
-  { slug: "royal-enfield-dealer-kuriachira", name: "Kuriachira" },
-  { slug: "royal-enfield-dealer-patturaikkal", name: "Patturaikkal" },
-  { slug: "royal-enfield-dealer-vadakkencherry", name: "Vadakkencherry" },
-  { slug: "royal-enfield-dealer-thriprayar", name: "thriprayar" },
-  { slug: "royal-enfield-dealer-kunnamkulam", name: "kunnamkulam" }
+  { slug: "royal-enfield-showroom-chalakudy", name: "Chalakudy" },
+  { slug: "royal-enfield-showroom-irinjalakuda", name: "Irinjalakuda" },
+  { slug: "royal-enfield-showroom-kodakara", name: "Kodakara" },
+  { slug: "royal-enfield-showroom-kuriachira", name: "Kuriachira" },
+  { slug: "royal-enfield-showroom-patturaikkal", name: "Patturaikkal" },
+  { slug: "royal-enfield-showroom-vadakkencherry", name: "Vadakkencherry" },
+  { slug: "royal-enfield-showroom-thriprayar", name: "thriprayar" },
+  { slug: "royal-enfield-showroom-kunnamkulam", name: "kunnamkulam" }
 ];
